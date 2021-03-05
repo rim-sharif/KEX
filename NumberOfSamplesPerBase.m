94b6efaff12353b837738e2ffecf6c57fc7bdeb5
rim-sharif <79467862+rim-sharif@users.noreply.github.com>
2021-03-05 18:02:51 +0100

    Update NumberOfSamplesPerBase.m

--- NumberOfSamplesPerBase.m
+++ NumberOfSamplesPerBase.m
@@ -4,8 +4,8 @@ vec = [];
 t1 = Ref_to_signal(1);
 
 for s = 2:length(Ref_to_signal)
-    antal_sampel = Ref_to_signal(s) - t1;
-    vec = [vec, antal_sampel];  
+    numberofsamples = Ref_to_signal(s) - t1;
+    vec = [vec, numberofsamples];  
     t1 = Ref_to_signal(s);      %update start point to "next" bar
 end
 
